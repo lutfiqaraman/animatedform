f7b2c0b7b081ae91df0943fe50f1e0a3cfbb445e
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-24 23:02:19 +0300

    write the HTML code

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<!-- HTNML Code -->
+<main>
+  <form>
+    <div class="field">
+      <input type="email" name="email" class="input" placeholder="" >
+      <label for="email" class="label">Email</label>
+    </div>
+
+    <div class="field">
+      <input type="password" name="password" class="input" placeholder="" >
+      <label for="password" class="label">Password</label>
+    </div>
+
+  </form>
+</main>
